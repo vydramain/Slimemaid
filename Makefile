dadaf97b1e21d3ce5d85be07aeb5f82942525049
vydra <vydramain@gmail.com>
2022-02-07 02:23:07 +0600

    First try with GLFW [NOt andROID]

---
+++ Makefile
@@ -0,0 +1,13 @@
+CFLAGS =  -std=c++17 -O2
+LDFLAGS = -lglfw -lvulkan -ldl -lpthread -lX11 -lXrandr -lXi
+Slimemaid: main.cpp 
+	g++ $(CFLAGS) -o Slimemaid main.cpp $(LDFLAGS)
+
+.PHONY: test clean
+
+test:	Slimemaid
+	./Slimemaid
+
+clean:
+	rm -rf Slimemaid
+
---
+++ main.cpp
@@ -0,0 +1,58 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <cstdio>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanUp();
+	}
+
+private:
+	GLFWwindow* window;
+
+	void initWindow() {
+		glfwInit();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	void initVulkan() {
+	}
+
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
+
+	void cleanUp() {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+};
+
+int main() {
+	HelloTriangleApplication app;
+
+	try {
+		app.run();
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return 	EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
