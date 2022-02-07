427950432100740bc904482c9084332c8267712f
vydra <vydramain@gmail.com>
2022-02-07 14:25:20 +0600

    Function for Vulkan instance initializing proces [NOt andROID]

--- main.cpp
+++ main.cpp
@@ -1,25 +1,57 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <iostream>
 #include <stdexcept>
+#include <iostream>
 #include <cstdlib>
 #include <cstdio>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
-public:
-	void run() {
-		initWindow();
-		initVulkan();
-		mainLoop();
-		cleanUp();
-	}
-
 private:
 	GLFWwindow* window;
+	VkInstance instance;
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Slimemaid";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo instanceCreateInfo{};
+        instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        instanceCreateInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        instanceCreateInfo.enabledExtensionCount = glfwExtensionCount;
+        instanceCreateInfo.ppEnabledExtensionNames = glfwExtensions;
+        instanceCreateInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&instanceCreateInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create Vulkan instance!");
+        } else {
+            std::cout << "Vulkan instance creation process ends with success..." << std::endl;
+        }
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "Available extensions:\n";
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+    }
 
 	void initWindow() {
 		glfwInit();
@@ -30,6 +62,7 @@ private:
 	}
 
 	void initVulkan() {
+		createInstance();
 	}
 
 	void mainLoop() {
@@ -39,9 +72,18 @@ private:
 	}
 
 	void cleanUp() {
+        vkDestroyInstance(instance, nullptr);
 		glfwDestroyWindow(window);
 		glfwTerminate();
 	}
+
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanUp();
+    }
 };
 
 int main() {
