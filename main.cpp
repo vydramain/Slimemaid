0701aa7d6fcdbabfe7ac975f4c7b40635d22ed06
vydra <vydramain@gmail.com>
2022-02-18 01:50:39 +0600

    Added to main debug describing variable

--- main.cpp
+++ main.cpp
@@ -10,6 +10,16 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
 class HelloTriangleApplication {
 private:
 	GLFWwindow* window;
