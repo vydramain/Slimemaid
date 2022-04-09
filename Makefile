e39e6679b6cd674159b982f9270a5b864f9ce404
vydra <vydramain@gmail.com>
2022-04-10 02:32:53 +0600

    Created staging buffer

--- Makefile
+++ Makefile
@@ -2,16 +2,19 @@ STB_INCLUDE_PATH = ./libraries/stb
 LDFLAGS = -lglfw -lvulkan -ldl -lpthread -lX11 -lXrandr -lXi
 CFLAGS = -std=c++17 -O2 -I$(STB_INCLUDE_PATH)
 
-compile_shaders: ./shaders/*
-	./compile_shaders.sh
-
 clean:
 	rm -rf ./build
 
-build:	clean compile_shaders ./source/main.cpp
+compile_shaders: ./shaders/*
+	./compile_shaders.sh
+
+prepare_raws: ./raws/*
+	cp -r ./raws ./build/raws
+
+build:	clean compile_shaders prepare_raws ./source/main.cpp
 	mkdir -p build && g++ $(CFLAGS) -o ./build/Slimemaid ./source/main.cpp $(LDFLAGS) -g
 
-.PHONY: clean compile_shaders test
+.PHONY: clean compile_shaders prepare_raws test
 
 test:	build
 	clear && cd ./build && ./Slimemaid
--- source/main.cpp
+++ source/main.cpp
@@ -314,22 +314,22 @@ private:
 
         VkDeviceSize imageSize = textureWidth * textureHeight * 4;
         if (!pixels) {
-            throw std::runtime_error("Failed to load raw texture " + texturePath);
+            throw std::runtime_error("Failed to load raw texture from: " + texturePath);
         }
 
-        VkBuffer stagingBuffer;
-        VkDeviceMemory stagingBufferMemory;
-
-        createBuffer(imageSize,
-                     VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-                     VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-                     stagingBuffer,
-                     stagingBufferMemory);
-
-        void* data;
-        vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data);
-            memcpy(data, pixels, static_cast<size_t>(imageSize));
-        vkUnmapMemory(device, stagingBufferMemory);
+        // VkBuffer stagingBuffer;
+        // VkDeviceMemory stagingBufferMemory;
+        //
+        // createBuffer(imageSize,
+        //              VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        //              VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        //              stagingBuffer,
+        //              stagingBufferMemory);
+        //
+        // void* data;
+        // vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data);
+        //     memcpy(data, pixels, static_cast<size_t>(imageSize));
+        // vkUnmapMemory(device, stagingBufferMemory);
 
         stbi_image_free(pixels);
     }
